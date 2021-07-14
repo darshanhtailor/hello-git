4f8cb4ba1618d9520ea30de705f97f6235111c3c
Darshan Tailor <darshanhtailor@gmail.com>
2021-07-15 02:46:31 +0530

    initial commit

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define FOR for (int i = 0; i < n; i++)
+#define IO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+int main()
+{
+    // IO
+    int n;
+    cin >> n;
+    cout << "Hello " << n;
+    return 0;
+}
